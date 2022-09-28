779004790e1036a8c05f25fef705060b13087815
Jorge Camargo <KuraiRantan>
2022-09-27 19:17:15 -0500

    Agregar componentes de landing page

---
+++ frontEnd/src/components/Footer/Footer.jsx
@@ -0,0 +1,93 @@
+import linkedinImg from '../../assets/linkedin.png';
+
+const Footer = () => {
+
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h3>DESARROLLADORES</h3>
+                <ul>
+                    <li>
+                        <a href="" target="_blank" >
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Carlos Mejía
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Eliz Longart
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Erick Seis
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Jonatan Di Vincenzo
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Jorge Camargo
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Ricardo Jorge
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Stiven Morales
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Yeminson Sanchez
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3>DISEÑADORA</h3>
+                <ul>
+                    <li>
+                        <a href="" target="_blank">
+                            <img src={linkedinImg} alt="linkedin icono" />
+                            Maria Galeano
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3>EXPLORA</h3>
+                <span>Ingresar</span>
+            </div>
+            <div>
+                <p>REALIZADO PARA FASE 1 DEL COHORTE 7 - NO COUNTRY TECH</p>
+            </div>
+        </div>
+    );
+};
+
+
+export default Footer;
---
+++ frontEnd/src/components/Modal/ModalLogin.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+const modalRootElement = document.getElementById('register-login-modal-root');
+
+const ModalLogin = ({ children }) => {
+    const newElement = document.createElement('div');
+
+    useEffect(() => {
+        modalRootElement.appendChild(newElement);
+        return () => modalRootElement.removeChild(newElement);
+    }, []);
+
+    return createPortal(children, newElement);
+};
+
+
+export default ModalLogin;

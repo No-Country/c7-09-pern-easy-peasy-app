be7af422ba4f7c3184a8dbe6f19cdc1e1e80d30b
ElizLongart <longartcoll@gmail.com>
2022-09-28 00:56:54 -0500

    ajustes de redux toolkit

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,11 +1,17 @@
 import { LayoutApp } from './layouts/LayoutApp'
 import { RoutesApp } from './routes/RoutesApp'
 
+//redux
+import { Provider } from 'react-redux'
+import { store } from './Store'
+
 function App() {
   return (
+    <Provider store={store}>
     <LayoutApp>
       <RoutesApp />
     </LayoutApp>
+    </Provider>
   )
 }
 
---
+++ frontEnd/src/slices/users/index.jsx
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+//axios
+import axios from 'axios';
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState: {
+        list: []
+    },
+    reducers: {
+        setUserList: (state, action) => {
+            state.list = action.payload;
+        }
+    },
+});
+
+export const { setUserList } = userSlice.actions;
+
+//está pendiente llamar los datos que estan en la DB
+
+export default userSlice.reducer;
+

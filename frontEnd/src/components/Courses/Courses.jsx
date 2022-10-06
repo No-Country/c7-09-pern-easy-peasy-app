39c767b416b0403ed63a203ac4ee652f5bc87a09
ElizLongart <longartcoll@gmail.com>
2022-10-05 21:33:12 -0500

    pull

--- frontEnd/src/components/Courses/Courses.jsx
+++ frontEnd/src/components/Courses/Courses.jsx
@@ -1,16 +1,21 @@
 import { dataCards } from '../../data/data'
 // css
 
-
 const Courses = () => {
   return (
     <div>
-      <div class='flex items-center'>
-        <label htmlFor="select-order" class='inline-block align-middle pr-2 pb-2'>Ordenado por:</label>
-        <select 
-        name="" 
-        id="select-order" 
-        class="bg-white hover:bg-gray-100 w-1/6 elementsm:w-3/4 md:w-20 lg:w-2 xl:w-1/4 text-gray-500 font-bold-light text-lg- italic py-2 px-4 pb-2 shadow-md border border-gray-300 rounded-full">
+      <div class="flex items-center">
+        <label
+          htmlFor="select-order"
+          class="inline-block align-middle pr-2 pb-2"
+        >
+          Ordenado por:
+        </label>
+        <select
+          name=""
+          id="select-order"
+          class="bg-white hover:bg-gray-100 w-1/6 elementsm:w-3/4 md:w-20 lg:w-2 xl:w-1/4 text-gray-500 font-bold-light text-lg- italic py-2 px-4 pb-2 shadow-md border border-gray-300 rounded-full"
+        >
           <option value="rec">Recomendados</option>
           <option value="pop">Populares</option>
           <option value="date">Fecha de actualización</option>
@@ -18,22 +23,21 @@ const Courses = () => {
         </select>
       </div>
       <div class="grid grid-cols-3 gap-4 pt-8">
-      {dataCards.length === 0 ? (
-        <p>There are no courses</p>
-      ) : (
-        dataCards.map((course) => (
-          <div
-            key={course.id}>
-            <img src={course.img} alt={course.name} />
-            <p>{course.name}</p>
-            <p>{course.description}</p>
-            <div>
-              <span>{course.punctuation}</span>
-              <span>${course.price} ARS</span>
+        {dataCards.length === 0 ? (
+          <p>There are no courses</p>
+        ) : (
+          dataCards.map((course) => (
+            <div key={course.id}>
+              <img src={course.img} alt={course.name} />
+              <p>{course.name}</p>
+              <p>{course.description}</p>
+              <div>
+                <span>{course.punctuation}</span>
+                <span>${course.price} ARS</span>
+              </div>
             </div>
-          </div>
-        ))
-      )}
+          ))
+        )}
       </div>
     </div>
   )

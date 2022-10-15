fa6f4697c2863898dc6b74025a67acfc1bce4632
Yeminson Sanchez <yeminsons@gmail.com>
2022-10-14 22:21:17 -0300

    add chance on server.js

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 require('dotenv').config()
-// const fs = require('fs')
-// const path = require('path')
 
 const app = express()
 
@@ -13,14 +11,6 @@ app.use(logger('dev'))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-// fs.createWriteStream(path.join(__dirname, '../tmp', 'access.log'), {
-//   flags: 'a',
-// })
-
-// fs.createWriteStream(path.join(__dirname, '../tmp', 'hola.log'), {
-//   flags: 'a',
-// })
 
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT)

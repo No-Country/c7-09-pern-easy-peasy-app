d46dd271710c07b55e7d6957f854527f5afed4c2
Yeminson Sanchez <yeminsons@gmail.com>
2022-10-20 22:12:45 -0300

    add changes to remote

--- frontEnd/src/components/Courses/Courses.jsx
+++ frontEnd/src/components/Courses/Courses.jsx
@@ -44,53 +44,53 @@ const Courses = () => {
   if (isLoading) return <Loader />
 
   return (
-        <div className="px-[5px] w-full">
-              <Ordenamiento addFilter={addFilter} />
+    <div className="px-[5px] w-full">
+      <Ordenamiento addFilter={addFilter} />
 
-              <div className="mr-auto ml-auto w-[97%] flex flex-wrap gap-8 pt-8">
-                {coursesFilter.length === 0 ? (
-                  <p>There are no courses</p>
-                ) : (
-                  coursesFilter.map((course) => (
-                    <div
-                      key={course.id}
-                      className="px-4 min-w-[300px] max-w-[300px] flex flex-col rounded-[30px] shadow-[0_4px_10px_rgba(0,0,0,0.25)] border-top-left-radius-20"
-                    >
-                        <div className="basis-full bg-white max-w-[280px] max-h-[216px] min-h-[216px] ">
-                          <img src={course.image_url} alt={course.title} />
-                        </div>
-                        <div className="basis-full flex flex-col">
-                          <hr />
-                          <div className="basis-full font-medium">
-                            <p className="font-family-poppins text-left text-lg tracking-normal text-sky-900">
-                              {course.title}
-                            </p>
-                            <p className="font-family-poppins text-left text-sm tracking-normal text-black">
-                              {course.description}
-                            </p>
-                          </div>
-                          <div className="basis-full flex justify-between ">
-                            <span className="mt-2 font-family-inter text-left text-xs text-sky-900 tracking-normal">
-                              {course.puntuaction}
-                            </span>
-                            <strong className="px-5px font-family-poppins text-right text-lg text-sky-900 tracking-normal">
-                              Valor: {course.price} $
-                            </strong>
-                          </div>
-                          <div className="basis-full flex justify-center items-end">
-                            <button
-                              type="button"
-                              className="h-[35px] w-[220px] mb-2 text-white bg-primary hover:bg-dark-primary dark:shadow-lg font-family-poppins rounded-full text-[16px] pb-2  py-2 text-center  "
-                            >
-                              Añadir al carrito
-                            </button>
-                          </div>
-                        </div>
-                    </div>
-                  ))
-                )}
+      <div className="mr-auto ml-auto w-[97%] flex flex-wrap gap-8 pt-8">
+        {coursesFilter.length === 0 ? (
+          <p>There are no courses</p>
+        ) : (
+          coursesFilter.map((course) => (
+            <div
+              key={course.id}
+              className="px-4 min-w-[300px] max-w-[300px] flex flex-col rounded-[30px] shadow-[0_4px_10px_rgba(0,0,0,0.25)] border-top-left-radius-20"
+            >
+              <div className="basis-full bg-white max-w-[280px] max-h-[216px] min-h-[216px] ">
+                <img src={course.image_url} alt={course.title} />
+              </div>
+              <div className="basis-full flex flex-col">
+                <hr />
+                <div className="basis-full font-medium">
+                  <p className="font-family-poppins text-left text-lg tracking-normal text-sky-900">
+                    {course.title}
+                  </p>
+                  <p className="font-family-poppins text-left text-sm tracking-normal text-black">
+                    {course.description}
+                  </p>
+                </div>
+                <div className="basis-full flex justify-between ">
+                  <span className="mt-2 font-family-inter text-left text-xs text-sky-900 tracking-normal">
+                    {course.puntuaction}
+                  </span>
+                  <strong className="px-5px font-family-poppins text-right text-lg text-sky-900 tracking-normal">
+                    Valor: {course.price} $
+                  </strong>
+                </div>
+                <div className="basis-full flex justify-center items-end">
+                  <button
+                    type="button"
+                    className="h-[35px] w-[220px] mb-2 text-white bg-primary hover:bg-dark-primary dark:shadow-lg font-family-poppins rounded-full text-[16px] pb-2  py-2 text-center  "
+                  >
+                    Añadir al carrito
+                  </button>
+                </div>
               </div>
             </div>
+          ))
+        )}
+      </div>
+    </div>
   )
 }
 export default Courses
